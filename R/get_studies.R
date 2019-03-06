b4da3555571fdb5facaac56758fd44954b87e877
wilsontom <wilsontom89@gmail.com>
2019-03-06 10:16:34 +0000

    GET all studies

---
+++ R/get_studies.R
@@ -0,0 +1,15 @@
+#' Get Studies
+#'
+#' @return a `tibble` of Study IDs
+#' @export
+
+get_studies <- function(){
+
+  studies <-
+    httr::GET(paste0(BASE_URL, '/studies')) %>% httr::content('parsed')
+
+  study_tibble <- tibble(unlist(studies))
+  names(study_tibble) <- 'study'
+
+  return(study_tibble)
+}
