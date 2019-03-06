b4a53e3db23752ca420c481d2192fb52c6bdac55
wilsontom <wilsontom89@gmail.com>
2019-03-06 11:17:52 +0000

    import dplyr functions

--- R/get_studies.R
+++ R/get_studies.R
@@ -8,7 +8,7 @@ get_studies <- function(){
   studies <-
     httr::GET(paste0(BASE_URL, '/studies')) %>% httr::content('parsed')
 
-  study_tibble <- tibble(unlist(studies))
+  study_tibble <- dplyr::tibble(unlist(studies))
   names(study_tibble) <- 'study'
 
   return(study_tibble)
