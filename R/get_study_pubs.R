63e816d4474815a3f20851aac5c9701fab823ecc
wilsontom <wilsontom89@gmail.com>
2019-03-07 14:48:07 +0000

    GET study publications

---
+++ R/get_study_pubs.R
@@ -0,0 +1,30 @@
+#' Get Study Publications
+#'
+#' @param study_id
+#' @return
+#' @export
+
+get_study_pubs <- function(study_id)
+{
+  study_pubs <-
+    httr::GET(
+      paste0(BASE_URL,
+             '/studies/',
+             study_id,
+             '/publications'),
+      httr::add_headers(user_token = getOption('MTBLS_API_KEY'))
+    )
+
+
+  study_pubs_parse <- study_pubs %>% httr::content('parsed')
+
+
+  study_pubs_tibble <- purrr::map(study_pubs_parse$publications, ~ {
+    unlist(.) %>% t() %>% dplyr::as_tibble()
+  }) %>% dplyr::bind_rows() %>% dplyr::select(-status.termAccession)
+
+  names(study_pubs_tibble)[5] <- 'status'
+
+  return(study_pubs_tibble)
+
+}
