25d4d2f9ef6f4403477b362f926b7f7835f95488
wilsontom <wilsontom89@gmail.com>
2019-03-07 14:36:45 +0000

    GET study factors

---
+++ R/get_study_factors.R
@@ -0,0 +1,38 @@
+#' Get Study Factors
+#'
+#' @param study_id
+#' @return
+#' @export
+
+get_study_factors <- function(study_id)
+{
+  study_fcts <-
+    httr::GET(
+      paste0(BASE_URL,
+             '/studies/',
+             study_id,
+             '/factors'),
+      httr::add_headers(user_token = getOption('MTBLS_API_KEY'))
+    )
+
+
+  study_fcts_parse <- study_fcts %>% httr::content('parsed')
+
+  study_fcts_list <- list()
+  for (i in seq_along(study_fcts_parse$factors)) {
+    study_fcts_list[[i]] <-
+      dplyr::tibble(
+        name = study_fcts_parse$factors[[i]]$factorName,
+        annotationValue = study_fcts_parse$factors[[i]]$factorType$annotationValue,
+        sourceName = study_fcts_parse$factors[[i]]$factorType$termSource$name,
+        sourceFile = study_fcts_parse$factors[[i]]$factorType$termSource$file,
+        sourceDescription = study_fcts_parse$factors[[i]]$factorType$termSource$description,
+        accession = study_fcts_parse$factors[[i]]$factorType$termAccession
+      )
+  }
+
+  study_fcts_tibble <- study_fcts_list %>% dplyr::bind_rows()
+
+  return(study_fcts_tibble)
+}
+
