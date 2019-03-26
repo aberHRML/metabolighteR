919535f3c6e602fa82762ae104d2688a00967e28
wilsontom <wilsontom89@gmail.com>
2019-03-26 10:47:40 +0000

    format

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,8 @@
   function(libname = find.package('metabolighteR'),
            pkgname = 'metabolighteR')
   {
-
-    assign('BASE_URL', 'https://www.ebi.ac.uk:443/metabolights/ws', env = .GlobalEnv)
+    assign('BASE_URL',
+           'https://www.ebi.ac.uk:443/metabolights/ws',
+           env = .GlobalEnv)
 
   }
-
-
