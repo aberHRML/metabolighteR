e1ffebb30a3c319f7dde17799ce531606c21dc06
wilsontom <wilsontom89@gmail.com>
2019-11-18 15:01:54 +0000

    remove redundant html parsing

--- R/get_isa_investigation.R
+++ R/get_isa_investigation.R
@@ -8,10 +8,12 @@ get_isa_investigation <- function(study_id, outpath)
 {
   isa_investigation <-
     httr::GET(
-      paste0(BASE_URL,
-             '/studies/',
-             study_id,
-             '/investigation'),
+      paste0(
+        getOption('BASE_URL'),
+        '/studies/',
+        study_id,
+        '/investigation'
+      ),
       httr::add_headers(user_token = getOption('MTBLS_API_KEY'))
     )
 
@@ -19,10 +21,8 @@ get_isa_investigation <- function(study_id, outpath)
   isa_investigation_parse <-
     isa_investigation %>% httr::content('text')
 
-  isa_clean <-
-    rvest::html_text(xml2::read_html(isa_investigation_parse))
-
-  writeLines(isa_clean, con = paste0(outpath, '/', study_id, '_ISA.txt'))
+  writeLines(isa_investigation_parse,
+             con = paste0(outpath, '/', study_id, '_ISA.txt'))
 
   return(invisible(NULL))
 }
