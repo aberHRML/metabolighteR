3c09d0fe56c879411373f69c0cf7260552a00d0e
wilsontom <wilsontom89@gmail.com>
2019-11-18 14:51:04 +0000

    update tests

--- tests/testthat/test-tokenless.R
+++ tests/testthat/test-tokenless.R
@@ -2,8 +2,7 @@ context('no-token-needed')
 
 test_that('tokenless-calls', {
 
-
-  expect_true(tibble::is.tibble(get_studies()))
+  expect_true(tibble::is_tibble(get_studies()))
   expect_true(is.list(get_webservice()))
 
 })
