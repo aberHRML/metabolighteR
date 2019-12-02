19c6763044b8efa19c46794d0108b14f1535365c
wilsontom <wilsontom89@gmail.com>
2019-12-02 10:34:31 +0000

    fix return for files

--- R/get_study_files.R
+++ R/get_study_files.R
@@ -1,5 +1,7 @@
 #' Get Study Files
 #'
+#' Retrieve the names and type of all files that have been deposited with the public study. If `raw_data` is TRUE then details of raw data files(ie, `.mzML`) are also returned.
+#'
 #' @param study_id A character string of a valid MTBLS study id
 #' @param raw_data logical; if `TRUE` then raw data file info is also returned. (Default is `FALSE`)
 #' @return a `tibble` of file info
@@ -41,7 +43,7 @@ get_study_files <- function(study_id, raw_data = FALSE)
 
   study_files_content <-  study_files %>% httr::content('parsed')
   study_files_tibble <-
-    purrr::map(study_files_content$studyFiles, dplyr::as_tibble) %>% dplyr::bind_rows()
+    purrr::map(study_files_content$study, dplyr::as_tibble) %>% dplyr::bind_rows()
 
   return(study_files_tibble)
 
