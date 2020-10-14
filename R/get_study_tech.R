6780c15fd16e0cb83da1370819df034cee720f8f
wilsontom <wilsontom89@gmail.com>
2020-10-14 17:49:26 +0100

    null finder

--- R/get_study_tech.R
+++ R/get_study_tech.R
@@ -16,16 +16,23 @@ get_study_tech <- function()
 
   study_tech_parse <- study_tech %>% httr::content('parsed')
 
-  study_tech_tibble <-
-    tibble::tibble(
-      STUDY = purrr::map_chr(study_tech_parse, ~ {
-        .[[1]]
-      }),
-      TECH = purrr::map_chr(study_tech_parse, ~
-                              {
-                                .[[2]]
-                              })
-    )
+
+  for (i in seq_along(study_tech_parse)) {
+    if (is.null(study_tech_parse[[i]][[2]])) {
+      study_tech_parse[[i]][[2]] <- 'NA'
+    }
+  }
+
+
+  study_tech_tibble <-  tibble::tibble(
+    STUDY = purrr::map_chr(study_tech_parse, ~ {
+      .[[1]]
+    }),
+    TECH = purrr::map_chr(study_tech_parse, ~
+                            {
+                              .[[2]]
+                            })
+  )
 
   return(study_tech_tibble)
 
