36c658a253484ac8fa8cedf5248348b307bbfd9e
wilsontom <wilsontom89@gmail.com>
2023-02-24 10:09:12 +0000

    add more token methods tests

--- tests/testthat/test-token-methods.R
+++ tests/testthat/test-token-methods.R
@@ -4,6 +4,8 @@ test_that('token-get-methods', {
   if (!is.null(getOption('MTBLS_API_KEY'))) {
     expect_true(tibble::is_tibble(get_study_org('MTBLS375')))
     expect_true(is.character(get_isa_investigation('MTBLS375')))
-  }
+    expect_true(is.list(get_study_audit('MTBLS375')))
+    expect_true(is.list(get_user_studies()))
+    }
 
 })
