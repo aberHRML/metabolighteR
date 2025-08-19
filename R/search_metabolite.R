d5542b64b827d6ed8b40528cf77ad4d4d12d489a
tpw2 <tpw2@aber.ac.uk>
2025-08-19 09:41:53 +0100

    add search metabolite

---
+++ R/search_metabolite.R
@@ -0,0 +1,29 @@
+#' Search Metabolite
+#'
+#' @param search_name A character string of a chemical name to search for
+#' @export
+#' @examples
+#' search_metabolite('proline')
+
+search_metabolite <- function(search_name)
+{
+  metabolite_search <-
+    httr::GET(glue::glue(getOption('BASE_URL'), '/search/name?search_value=', {
+      search_name
+    }))
+
+  metabolite_search_res <- metabolite_search %>% httr::content('parsed')
+
+
+  metabolite_tibble <-
+    tibble::tibble(
+      name = metabolite_search_res$content[[1]]$name,
+      inchi = metabolite_search_res$content[[1]]$inchi,
+      databaseId = metabolite_search_res$content[[1]]$databaseId,
+      formula = metabolite_search_res$content[[1]]$formula,
+      smiles = metabolite_search_res$content[[1]]$smiles,
+      search_resource = metabolite_search_res$content[[1]]$search_resource
+    )
+
+  return(metabolite_tibble)
+}
